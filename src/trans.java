18dbf000f456fe4b698a8ec88a785fad19ca94b6
Ronald Tudu <xgrape@outlook.com>
2017-06-23 20:50:59 +0600

    testing_1.3 setting up the superclass trans with default constructor
    Signed-off-by: Ronald Tudu <xgrape@outlook.com>

--- src/trans.java
+++ src/trans.java
@@ -1,6 +1,25 @@
+import java.util.*;
+import java.lang.*;
+
+
 /**
  * Created by xgrape on 6/23/17.
  */
 public class trans {
-    
+
+    public Calendar Search_date;
+    public String source;
+    public  String dest;
+
+
+    trans(Calendar Search_date, String source, String dest){
+
+        this.Search_date=Search_date;
+        this.source=source;
+        this.dest=dest;
+
+
+    }
+
+
 }
