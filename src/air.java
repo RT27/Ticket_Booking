2b533068a0ea5d41b2f9804d6ad790e28e6af58a
Ronald Tudu <xgrape@outlook.com>
2017-06-23 20:39:15 +0600

    testing_1.2 setting up classes
    Signed-off-by: Ronald Tudu <xgrape@outlook.com>

---
+++ src/air.java
@@ -0,0 +1,6 @@
+/**
+ * Created by xgrape on 6/23/17.
+ */
+public class air extends trans {
+
+}
---
+++ src/train.java
@@ -0,0 +1,6 @@
+/**
+ * Created by xgrape on 6/23/17.
+ */
+public class train extends trans {
+
+}
