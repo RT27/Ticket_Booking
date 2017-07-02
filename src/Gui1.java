fe02af0d233346970d3847dfe97de452d36a2ac8
Ronald Tudu <xgrape@outlook.com>
2017-07-02 22:16:21 +0600

    testing_1.3 setting up the superclass trans with default constructor
    Signed-off-by: Ronald Tudu <xgrape@outlook.com>

--- src/Gui1.java
+++ src/Gui1.java
@@ -1,5 +1,3 @@
-import javafx.scene.control.Alert;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,20 +12,53 @@ import java.text.*;
  */
 public class Gui1 extends trans{
     public JPanel panel1;
-    private JButton Searchbutton;
     private JTextField textField1;
     private JComboBox comboBoxOrigin;
-    private JComboBox comboBoxDestinatiom;
+    private JComboBox comboBoxDestination;
     private JButton Search;
 
+
+    trans t1 = new trans();
+
     public Gui1() {
-        Searchbutton.addActionListener(new ActionListener() {
+        Search.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String x=textField1.getText();
-                ChecknSendDate(x);
-                trans t = new trans ();
+                try{
+                    Date date = new SimpleDateFormat("dd.MM.yyyy").parse(x);
+                    System.out.println(date);
 
+                }
+                catch (Exception ex){
+                    ex.printStackTrace();
+
+                }
+
+
+               // t1.Search_date=
+
+
+
+
+
+
+            }
+        });
+        comboBoxOrigin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String origin= comboBoxOrigin.getSelectedItem().toString();
+                System.out.println(origin);
+
+            }
+        });
+        comboBoxDestination.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String dest= comboBoxDestination.getSelectedItem().toString();
+                System.out.println(dest);
 
 
             }
@@ -35,31 +66,31 @@ public class Gui1 extends trans{
     }
 
 
-    public  Calendar ChecknSendDate(String x){
+    public void ChecknSendDate(String x){
 
 
          try {
              SimpleDateFormat sdf = new SimpleDateFormat("dd.mm.yyyy");
              Date date = new Date();
              date = sdf.parse(x);
-             if (!x.equals(sdf.format(date))) {
-                 JOptionPane.showMessageDialog(null, "pls enter date in format dd.mm.yyyy", "Alert Message", JOptionPane.INFORMATION_MESSAGE);
-
-             } else {
+             if (x.equals(sdf.format(date))) {
                  JOptionPane.showMessageDialog(null, "match found", "Searching", JOptionPane.INFORMATION_MESSAGE);
 
+             } else {
 
+
+                 JOptionPane.showMessageDialog(null, "pls enter date in format dd.mm.yyyy", "Alert Message", JOptionPane.INFORMATION_MESSAGE);
 
 
              }
          } catch (ParseException ex) {
              ex.printStackTrace();
+             JOptionPane.showMessageDialog(null, "pls enter date in format dd.mm.yyyy", "Alert Message", JOptionPane.INFORMATION_MESSAGE);
 
          }
 
-        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-        Calendar cal = Calendar.getInstance();
 
-        return cal;
+
+
     }
 }
